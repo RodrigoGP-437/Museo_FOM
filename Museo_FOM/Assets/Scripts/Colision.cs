15d8462575db57c4c647179852540b7d97d58fcb
Rodrigo Gómez <rodrigogp4812@gmail.com>
2025-04-21 01:50:01 -0600

    Version_1

---
+++ Museo_FOM/Assets/Scripts/Colision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour
+{
+    public GameObject Mensaje;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Mensaje.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Mensaje.SetActive(false);
+        }
+    }
+}
---
+++ Museo_FOM/Assets/Scripts/Mensaje.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mensaje : MonoBehaviour
+{
+    public float TiempoDeVida = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TiempoDeVida -= Time.deltaTime;
+        if (TiempoDeVida <= 0)
+        {
+            this.gameObject.SetActive (false);
+            TiempoDeVida = 3;
+        }
+    }
+}
